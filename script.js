d6ffe0d1e976872fd013aaf1d3099599b58ed1f6
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2022-11-13 06:36:08 +0530

    new files added

--- script.js
+++ script.js
@@ -11,8 +11,43 @@ menu.addEventListener("click", () => {
     }
 });
 let remove = () => {
-    if (window.innerWidth <= 800){
+    if (window.innerWidth <= 800) {
         menuList.classList.toggle("show");
     }
     menu.classList.replace("close", "open");
 };
+
+localStorage.clear();
+let i = 1, j = 2, k = 3;
+let addData = () => {
+    let titleTxt = document.getElementById("title");
+    let messageTxt = document.getElementById("message");
+    let nmTxt = document.getElementById("name");
+    if (typeof (Storage) !== "undefined") {
+        localStorage.setItem(i, titleTxt.value);
+        localStorage.setItem(j, messageTxt.value);
+        localStorage.setItem(k, nmTxt.value);
+        i += 3;
+        j += 3;
+        k += 3;
+    }
+    else {
+        alert("Sorry, your browser does not support Web Storage !!");
+    }
+    titleTxt.value = messageTxt.value = nmTxt.value = "";
+};
+
+let valArr = {"Paytm": "98xxxxxxx0@paytm", "PhonePe": "elvesxxxxx@ybi", "Google Pay": "123456789@hdfcbank"};
+let showDetails = (val) => {
+    document.querySelector("#donate fieldset .app-img img").src = `${val}.svg`;
+    document.querySelector("#donate fieldset .account-data h6").textContent = valArr[val];
+};
+
+document.querySelector("#donate .redirect-btn").addEventListener("click", () => {
+    document.querySelector("#donate .donate-bx").classList.toggle("visible");
+    document.querySelector("#donate .redirect-btn").classList.toggle("hidden");
+});
+document.querySelector("#donate .done-btn").addEventListener("click", () => {
+    document.querySelector("#donate .donate-bx").classList.toggle("visible");
+    document.querySelector("#donate .redirect-btn").classList.toggle("hidden");
+});
